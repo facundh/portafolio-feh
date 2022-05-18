44c9b75cd9876d357a891cef9d0b45505922dd76
Facundo Herrera <facundhfed@gmail.com>
2022-05-18 17:12:42 +0200

    pagina works finalizada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,27 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-sky-500">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-white">
-          Facundo Herrera
-        </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-gray-500">
-          I'm a Junior React Developer
-        </h2>
-        <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-sky-300 hover:border-sky-300">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-4" />
-            </span>
-          </button>
-        </div>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+ 
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <p className='text-sky-600'>Hi, my name is</p>
+      <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        Facundo Herrera
+      </h1>
+      <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        I'm a Junior React Developer.
+      </h2>
+     
+      <div>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-sky-600 hover:border-sky-600'>
+          View Work
+          <span className='group-hover:rotate-90 duration-300'>
+            <HiArrowNarrowRight className='ml-3 ' />
+          </span>
+        </button>
       </div>
     </div>
+  </div>
   );
 };
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import cotizador from "../img/cotizadorCripto.jpg";
+import cotizador from "../img/cotizadov1.jpg";
+import compra from '../img/compraCripto.jpg';
+import game from '../img/colorGame.jpg';
+import songs from '../img/songs.jpg';
+import admin from '../img/admin.jpg';
+import vet from '../img/vet.jpg';
 
 const Work = () => {
   return (
@@ -15,12 +20,133 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
             style={{
-              backgroundImage: `url(${cotizador})`,
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              height: "650px",
-              width: "1300px",
+              backgroundImage: `url(${cotizador})`
+            
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${compra})`
+            
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${game})`
+            
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${songs})`
+            
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${admin})`
+            
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${vet})`
+            
             }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
